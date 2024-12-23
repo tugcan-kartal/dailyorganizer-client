afd3169df800af36483bd0a0c390fe200260b03d
tugcan-kartal <tugcankartal01@gmail.com>
2024-12-24 01:35:11 +0300

    connected db and users model added

---
+++ src/lib/connect.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB=async():Promise<void>=>{
+    try {
+        const dbUri=process.env.MONGO!;
+        if(!dbUri){
+            throw new Error("MongoDB URI is not defined in the environment variables.");
+        }
+
+        await mongoose.connect(dbUri);
+        console.log("Successfully connected to MongoDB");
+    } catch (error) {
+        console.log("Connection to MongoDB failed",error);
+        throw new Error("Connection failed!");
+    }
+}
+
+export default connectDB;
