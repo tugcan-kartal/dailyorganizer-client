78c2f6aa886b6e45c20dcea82cab346866ea72d7
tugcan-kartal <tugcankartal01@gmail.com>
2025-01-02 15:39:53 +0300

    updated

---
+++ src/app/components/getTask/getTask.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const GetTask:React.FC=()=>{
+
+    return(
+        <div>
+            
+        </div>
+    )
+
+}
+
+export default GetTask;
--- src/app/task/page.tsx
+++ src/app/task/page.tsx
@@ -2,13 +2,14 @@
 
 import React from "react";
 import AddTask from "../components/addTask/addTask";
+import GetTask from "../components/getTask/getTask";
 
 const Task: React.FC =() => {
 
     return(
         <div>
-            <AddTask />
-            
+            {/* <AddTask /> */}
+            <GetTask />
         </div>
     )
 
