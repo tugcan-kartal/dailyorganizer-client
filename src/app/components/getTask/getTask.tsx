fec34c8a077258c8dfb55f6b8810d21527654a64
tugcan-kartal <tugcankartal01@gmail.com>
2025-01-03 13:52:15 +0300

    can be update

--- src/app/components/getTask/getTask.tsx
+++ src/app/components/getTask/getTask.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 
 const GetTask: React.FC = () => {
 
@@ -6,6 +6,17 @@ const GetTask: React.FC = () => {
 
     const [errors, setErrors] = useState<string[]>([]);
     const [success, setSuccess] = useState<string>("");
+
+    const [updatedTask,setUpdatedTask]=useState({
+        title: "",
+        description: "",
+        author: "",
+        importance_level: "",
+        category: "",
+        start_date: "",
+        end_date: "",
+    })
+    const [openUpdate,setOpenUpdate]=useState(false);
 
     const getUserTasks=async()=>{
 
@@ -62,20 +73,90 @@ const GetTask: React.FC = () => {
 
     }
 
+    const handleUpdate=async(taskId: string)=>{
+
+        try {
+            const response=await fetch(`http://localhost:3000/task/${taskId}`,{
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(updatedTask),
+            })
+
+            if(!response.ok){
+                throw new Error("Couldnt send updated task");
+            }else{
+                const updatedSuccessfully=await response.json();
+                console.log(updatedSuccessfully);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleChange=(e: ChangeEvent<HTMLInputElement>)=>{
+        setUpdatedTask({
+            ...updatedTask,
+            [e.target.name]: e.target.value
+        });
+    };
+
     return(
         <div>
             <div>
                 {userTasks && userTasks.map((item:any,index:any)=>(
                     <div className="my-10 bg-gray-300" key={index}>
+
                         <div className="text-xl font-semibold">{item.title}</div>
+                        {
+                            openUpdate &&
+                            <input name="title" onChange={handleChange} placeholder="Edit Title"/>
+                        }
+
                         <div className="text-lg">{item.description}</div>
-                        <div className="text-gray-400">{item.author}</div>
-                        <div className="bg-green-400 p-2 inline-block rounded-full">{item.importance_level}</div>
+                        {
+                            openUpdate &&
+                            <input name="description" onChange={handleChange} placeholder="Edit Title"/>
+                        }
+
+                        <div className="">{item.author}</div>
+                        {
+                            openUpdate &&
+                            <input name="author" onChange={handleChange} placeholder="Edit Title"/>
+                        }
+
+                        <div className="">{item.importance_level}</div>
+                        {
+                            openUpdate &&
+                            <input name="importance_level" onChange={handleChange} placeholder="Edit Title"/>
+                        }
+
                         <div>{item.category}</div>
+                        {
+                            openUpdate &&
+                            <input name="category" onChange={handleChange} placeholder="Edit Title"/>
+                        }
+
                         <div>{new Date(item.start_date).toLocaleDateString("en-GB")}</div>
+                        {
+                            openUpdate &&
+                            <input type="date" name="start_date" onChange={handleChange} placeholder="Edit Title"/>
+                        }
+
                         <div>{new Date(item.end_date).toLocaleDateString("en-GB")}</div>
+                        {
+                            openUpdate &&
+                            <input type="date" name="end_date" onChange={handleChange} placeholder="Edit Title"/>
+                        }
 
                         <div onClick={()=>handleDelete(item._id)} className="text-red-700 text-xl cursor-pointer">X</div>
+                        <div onClick={()=>setOpenUpdate(!openUpdate)}>Edit</div>
+
+                        {
+                            openUpdate &&
+                            <div onClick={()=>handleUpdate(item._id)}>Update Task</div>
+                        }
                     </div>
                 ))}
             </div>
