8b9167522ac531d1fded6767e49fa1678fc3a055
tugcan-kartal <tugcankartal01@gmail.com>
2025-01-10 21:52:39 +0300

    sidebar icon added

--- src/app/components/sideBar.tsx
+++ src/app/components/sideBar.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import Image from "next/image";
 import CategoryBar from "@/../public/assets/sidebar-images/category.png";
 import ImportanceBar from "@/../public/assets/sidebar-images/importance.png";
 import StartDateBar from "@/../public/assets/sidebar-images/start-date.png";
 import EndDateBar from "@/../public/assets/sidebar-images/end-date.png";
-import Image from "next/image";
+import { GoSidebarExpand } from "react-icons/go";
+import { GoSidebarCollapse } from "react-icons/go";
 
 interface SideBarProps {
     setTaskFilter: (filter: string) => void;
@@ -11,9 +13,21 @@ interface SideBarProps {
 
 const SideBar: React.FC<SideBarProps> = ({ setTaskFilter }) => {
     
+    const [isSideBar,setIsSiteBar]=useState<boolean>(true)
+
     return (
         <div>
             <div>
+                <div className="flex justify-between pl-5 pt-4">
+                    <div className="text-2xl" onClick={()=>setIsSiteBar(!isSideBar)}>
+                        {isSideBar ? <GoSidebarExpand /> : <GoSidebarCollapse />}
+                    </div>
+
+                    <div>
+
+                    </div>
+                </div>
+
                 <div className="flex flex-col justify-center gap-y-[2vh] mt-[5vh] ml-[1vw] border-b-2 border-gray-300 pb-10">
 
                     <div className="cursor-pointer flex gap-x-4 w-[85%] hover:shadow-lg transition-all duration-300 px-2 py-2 rounded-lg" onClick={()=>setTaskFilter("category:asc")}>
@@ -47,9 +61,7 @@ const SideBar: React.FC<SideBarProps> = ({ setTaskFilter }) => {
 
                 </div>
 
-
                 <div>
-
                 </div>
             </div>
         </div>
