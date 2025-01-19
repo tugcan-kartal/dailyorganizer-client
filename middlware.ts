50ea188edb8d7d65273496bd435d4a0097fcc4bb
tugcan-kartal <tugcankartal01@gmail.com>
2025-01-19 15:21:38 +0300

    middleware updated

--- middlware.ts
+++ middlware.ts
@@ -1,5 +1,6 @@
 "use client"
 
+import { getUserTasks } from "@/app/api/taskService";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
@@ -10,7 +11,21 @@ const Middleware:React.FC=()=>{
         const token=localStorage.getItem("token");
 
         if(token){
-            router.push("/tasks");
+            try {
+                const waitForAsync=async()=>{
+                    const checking=await getUserTasks(token,"category:asc");
+                    if(checking.statusCode===401){
+                        localStorage.removeItem("token");
+                        router.push("/user/signup");
+                    }else{
+                        router.push("/tasks");
+                    }
+                }
+
+                waitForAsync();
+            } catch (error) {
+                console.log(error);
+            }
         }else{
             router.push("/user/signup");
         }
