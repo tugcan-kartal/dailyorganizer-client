da81d50e08d2650e5966fa8b0f7a5f253cd16908
tugcan-kartal <tugcankartal01@gmail.com>
2025-01-21 01:02:10 +0300

    updated design

--- src/app/components/headerActions.tsx
+++ src/app/components/headerActions.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { GoChecklist, GoSidebarCollapse } from "react-icons/go";
 import { IoCreateOutline } from "react-icons/io5";
-import { CiViewList } from "react-icons/ci";
 import { useTasksContext } from "../context/TasksContext";
-
+import Image from "next/image";
+import LogoTransparent from "@/../public/logo-transparent.png"
 const HeaderActions: React.FC = () => {
 
   const {
@@ -16,19 +16,33 @@ const HeaderActions: React.FC = () => {
   return (
     <div className="pl-4">
       {!isSideBar && (
-        <div className="flex gap-x-4 pt-[2vh]">
+        <div className="flex items-center gap-x-4 pt-[2vh]">
+
           <div
             onClick={() => setIsSideBar(!isSideBar)}
             className="text-2xl cursor-pointer"
           >
             <GoSidebarCollapse />
           </div>
+
           <div
             onClick={() => setIsAddTask(!isAddTask)}
             className="text-2xl cursor-pointer"
           >
             {!isAddTask ? <IoCreateOutline /> : <GoChecklist />}
           </div>
+          
+          <div className="flex items-center">
+            {/* İkon kısmı */}
+            {/* <div className="w-[5%]">
+              <Image src={LogoTransparent} alt="not found"/>
+            </div> */}
+            {/* Uygulama adı */}
+            <div className="text-lg font-semibold">
+              Taskly Adviser AI
+            </div>
+          </div>
+          
         </div>
       )}
     </div>
